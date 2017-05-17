c4e2939ab0283659916114d30728f71a22c15041
Aluan <aluan.cabral@outlook.com>
2017-05-17 11:54:11 -0300

    Primeiro Componente

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Component from './component'
+
 ReactDOM.render(
-    <h1>Olá React</h1>,
+    <Component />,
     document.getElementById('app')
 )
 
